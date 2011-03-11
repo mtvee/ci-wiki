65adf8a114a6f259aa71a05bfe2b7ccb6ced4c68
J Knight <emptyvee@gmail.com>
2011-03-11 14:52:38 -0500

    more comments

--- system/application/controllers/wiki.php
+++ system/application/controllers/wiki.php
@@ -10,18 +10,24 @@ class Wiki extends Controller
 		
     $this->load->model('wiki_model');
     $this->load->helper('textile');
-    $this->load->helper('url');
 		$this->load->library('creole');
 	}
 
-	
+	/**
+	 * this is the only publicly available method. The 2nd segment of the
+	 * url is the page name rather then the method. If you move the controller
+	 * deeper or shallower you have to adjust the offsets here. Operations are
+	 * the final element of the URL, for edit, history, etc and those methods
+	 * are protected.
+	 */
 	function _remap()
 	{
     $page_name = $this->uri->segment(2);
     if( !$page_name ) {
       $page_name = 'Index';
     }
-    
+
+    // figure out if there is an operation call
     $editing = false;
     if( $this->uri->segment(3,'') == 'edit') {
       $editing = true;
@@ -38,7 +44,8 @@ class Wiki extends Controller
       $this->diff( $page_name, $this->uri->segment(4) );
       return;
     }
-
+		
+		// handle data submission
     if( $this->input->post('save')) {
       $id = $this->input->post('id');
       $title = $this->input->post('title');
@@ -52,8 +59,10 @@ class Wiki extends Controller
       $editing = false;
     }
 
+		// find the page
     $page = $this->wiki_model->get_page( $page_name );
     
+		// page is empty, so edit it automatically
     if( !$page ) {
       $page = new StdClass();
       $page->id = -1;
@@ -74,26 +83,29 @@ class Wiki extends Controller
 				} 
       }
     }
-
+		
+		// data for the view
 		$view_data = array(
       'page' => $page,
       'errors' => ''
 			);
-
+		// content is conditional on edit operation
     if( $editing ) {
 			$content = $this->load->view('wiki/page_edit', $view_data, true );
     } else {
 			$content = $this->load->view('wiki/page_view', $view_data, true );
     }
 
+		// data for the layout view
 		$pg_data = array(
 			'content' => $content,
 			'page_title' => 'CI-Wiki - ' . $page->title
 		);
-
+		// render
 		$this->load->view('layouts/standard_page', $pg_data );
 	}
 
+	// generate the history view for a given page
   protected function history( $page_name )
   {
     $page = $this->wiki_model->get_page( $page_name );
@@ -123,6 +135,7 @@ class Wiki extends Controller
 		
   }
 
+	// generate the diff view for a given page revision
 	protected function diff( $page_name, $id )
 	{
     $view_data = array(
--- system/application/views/layouts/standard_page.php
+++ system/application/views/layouts/standard_page.php
@@ -1,60 +1,59 @@
 <html>
 <head>
-<title><?= $page_title ?></title>
+  <title><?= $page_title ?></title>
+  <style type="text/css">
 
-<style type="text/css">
+	body {
+	 background-color: #fff;
+	 margin: 40px;
+	 font-family: Lucida Grande, Verdana, Sans-serif;
+	 font-size: 14px;
+	 color: #4F5155;
+	}
 
-body {
- background-color: #fff;
- margin: 40px;
- font-family: Lucida Grande, Verdana, Sans-serif;
- font-size: 14px;
- color: #4F5155;
-}
+	a {
+	 color: #003399;
+	 background-color: transparent;
+	 font-weight: normal;
+	}
 
-a {
- color: #003399;
- background-color: transparent;
- font-weight: normal;
-}
+	h1 {
+	 color: #444;
+	 background-color: transparent;
+	 border-bottom: 1px solid #D0D0D0;
+	 font-size: 16px;
+	 font-weight: bold;
+	 margin: 24px 0 2px 0;
+	 padding: 5px 0 6px 0;
+	}
 
-h1 {
- color: #444;
- background-color: transparent;
- border-bottom: 1px solid #D0D0D0;
- font-size: 16px;
- font-weight: bold;
- margin: 24px 0 2px 0;
- padding: 5px 0 6px 0;
-}
+	code {
+	 font-family: Monaco, Verdana, Sans-serif;
+	 font-size: 12px;
+	 background-color: #f9f9f9;
+	 border: 1px solid #D0D0D0;
+	 color: #002166;
+	 display: block;
+	 margin: 14px 0 14px 0;
+	 padding: 12px 10px 12px 10px;
+	}
 
-code {
- font-family: Monaco, Verdana, Sans-serif;
- font-size: 12px;
- background-color: #f9f9f9;
- border: 1px solid #D0D0D0;
- color: #002166;
- display: block;
- margin: 14px 0 14px 0;
- padding: 12px 10px 12px 10px;
-}
+	a {
+		text-decoration: none;
+	}
 
-a {
-	text-decoration: none;
-}
-
-#wiki-tools {
-	float: right;
-}
-
-span.ins {
-	display: block; background-color: #9f9;
-}
-
-span.del {
-	display: block; background-color: #f99;
-}
-</style>
+	#wiki-tools {
+		float: right;
+	}
+	
+	/* diff */
+	span.ins {
+		display: block; background-color: #9f9;
+	}
+	span.del {
+		display: block; background-color: #f99;
+	}
+  </style>
 </head>
 <body>
 	<?= $content ?>
