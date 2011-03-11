a1a9462c4e07d2f310be7954244d6bc2bc045a6f
J Knight <emptyvee@gmail.com>
2011-03-11 11:34:20 -0500

    remove edit help

--- system/application/views/wiki/page_edit.php
+++ system/application/views/wiki/page_edit.php
@@ -1,45 +1,6 @@
 
 <h3><?=$page->title?></h3>
-<div style="float: right; width: 30%; background-color: #eee;">
-  <table cellpadding="5px">
-    <tr>
-      <td>h#. <em>text</em></td>
-      <td>headings &lt;h#&gt;</td>
-    </tr>
-    <tr>
-      <td>_<em>text</em>_</td>
-      <td>italic &lt;em&gt;</td>
-    </tr>
-    <tr>
-      <td>*<em>text...</em>*</td>
-      <td>bold &lt;strong&gt;</td>
-    </tr>
-    <tr>
-      <td>+<em>text...</em>+</td>
-      <td>underline &lt;u&gt;</td>
-    </tr>
-    <tr>
-      <td>pre. <em>text...</em></td>
-      <td>preformatted &lt;pre&gt;</td>
-    </tr>
-    <tr>
-      <td>"<em>text</em>":<em>url</em></td>
-      <td>link</td>
-    </tr>
-    <tr>
-      <td>!<em>url</em>!</td>
-      <td>image</td>
-    </tr>
-    <tr>
-      <td>*<em>text</em></td>
-      <td>bullet (unordered) list</td>
-    </tr>
-    <tr>
-      <td>#<em>text</em></td>
-      <td>numbered (ordered) list</td>
-    </tr>
-  </table>
-</div>
+
 <?= $errors ?>
 <form method="post" action="<?=site_url()?>/wiki/<?=$page->title?>">
   <input type="hidden" name="id" value="<?=$page->id?>" />
